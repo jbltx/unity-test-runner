a2c2aaeab823b8d5d88f970e1a773a61dc89b0cb
Mickael Bonfill <jbltx@protonmail.com>
2021-02-08 11:50:53 -0500

    Fix sed command to insert code coverage package

--- action/steps/check_coverage_support.sh
+++ action/steps/check_coverage_support.sh
@@ -8,17 +8,27 @@ UNITY_VERSION_MAJOR=${semver%%\.*}
 UNITY_VERSION_MINOR=${semver#*.}
 UNITY_VERSION_MINOR=${UNITY_VERSION_MINOR%.*}
 
+CODE_COVERAGE_PACKAGE_NAME="com.unity.testtools.codecoverage"
+CODE_COVERAGE_PACKAGE_VERSION="0.4.0-preview.6"
+
 if [[ -z "$UNITY_VERSION_MAJOR" || -z "$UNITY_VERSION_MINOR" ]]; then 
     echo "Unable to parse the Unity version"
     COVERAGE_UNSUPPORTED=1
 else
     if [[ $UNITY_VERSION_MAJOR -gt 2019 || ( $UNITY_VERSION_MAJOR -eq 2019 && $UNITY_VERSION_MINOR -gt 2 ) ]]; then
         echo "Checking manifest file for coverage package at \"$MANIFEST_PATH\"."
-        cat "$MANIFEST_PATH" | grep -q "com.unity.testtools.codecoverage"
+        cat "$MANIFEST_PATH" | grep -q "$CODE_COVERAGE_PACKAGE_NAME"
         coverage_package_present=$?
         if [[ $coverage_package_present -gt 0 ]]; then 
             echo "Adding Coverage package manually in the manifest."
-            sed 's/,/, "com.unity.testtools.codecoverage":"0.3.1-preview",/1' "$MANIFEST_PATH"
+            regex_pattern=$( printf '0,/,/s//,\\n    "%s":"%s",/' "$CODE_COVERAGE_PACKAGE_NAME" "$CODE_COVERAGE_PACKAGE_VERSION" )
+            sed "$regex_pattern" "$MANIFEST_PATH" > "$MANIFEST_PATH"
+            cat "$MANIFEST_PATH" | grep -q "$CODE_COVERAGE_PACKAGE_NAME"
+            coverage_package_present=$?
+            if [[ $coverage_package_present -gt 0 ]]; then 
+              echo "Unable to insert package into the Unity Project manifest file \"$MANIFEST_PATH\""
+              COVERAGE_UNSUPPORTED=1
+            fi
         fi
     else 
         echo "Unity version $UNITY_VERSION doesn't support the Coverage package. Coverage requires Unity 2019.3+"
